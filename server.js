fb738eecacd05fef51112cf31e65cf21ea99b213
yankvasya <yankvasya@gmail.com>
2022-02-23 12:41:03 +0300

    refactor

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 const http = require('http');
 
 const port = 8080;
-const interval = 1000;
-let timer = 10000;
+const interval = process.env.interval || 1000;
+let timer = process.env.timer || 10000;
 
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-type': 'text/html' });
 
   if (req.method === 'GET') {
     const currentTimer = setInterval(() => {
-      const data = new Date();
+      if (timer > 0) {
+        console.log(`${new Date()}`);
+      } else if (timer === 0) {
+        console.log(`Stopped at - ${new Date()}`);
+        clearInterval(currentTimer);
+        res.end();
+      }
 
       timer -= interval;
-
-      if (timer <= 0) {
-        if (timer === 0) {
-          res.end(`<div>Server stopped at - ${data}`);
-          console.log(`Server stopped at - ${data}`);
-        }
-        return clearInterval(currentTimer);
-      } else {
-        console.log(`${data}`);
-        res.write(`<div>${data}`);
-      }
     }, interval);
   }
 }).listen(port, () => console.log(`HTTP server running on port ${port}`));
